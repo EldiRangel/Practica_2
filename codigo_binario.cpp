8b451f7b7e0d730192fb9a14700dbf2948acc0e4
eldi <eldi456.rangel@gmail.com>
2024-10-29 19:40:43 -0700

    parte 1

---
+++ codigo_binario.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+struct data
+{int id_registro;
+char nombre[35];
+char cedula[9];
+int edad;
+float peso;
+float altura;
+string genero;
+};
+int main(){
+int number_persons;
+cout <<"ingrese el numero de persoas:";
+cin.ignore();
+cin >> number_persons;
+data personas [number_persons];
+
+
+
+
+
+
+
+}
