b2611ede25966a12f887454c1cde78c740c2d7b1
eldi <eldi456.rangel@gmail.com>
2024-10-30 15:15:01 -0700

    version_final

--- codigo_binario.cpp
+++ codigo_binario.cpp
@@ -3,9 +3,10 @@
 #include<fstream>
 using namespace std;
 struct data
-{int id_registro;
-char nombre[35];
-char cedula[9];
+{
+char id_registro[50];
+char nombre[30];
+char cedula[10];
 int edad;
 float peso;
 float altura;
@@ -13,26 +14,27 @@ string genero;
 };
 int main(){
 int number_persons;
-cout <<"ingrese el numero de persoas:";
-cin.ignore();
+cout <<"ingrese el numero de personas: ";
 cin>> number_persons;
 data personas [number_persons];
  for (int i = 0; i < number_persons; i++)
-    { cout<< "persona"<< i+1<<".\n";
-      cout<< "id de; registo";
+    { cout<< "persona: "<< i+1<<".\n";
+      cout<< "id de registo: ";
       cin>>personas[i].id_registro;
-      cout<< "nombre"<< endl;
-      cin.getline(personas[i].nombre,35);
-      cout<< "cedula"<< endl;
-      cin.getline(personas[i].cedula,9);
-      cout<< "edad";
+      cout<< "Nombre: ";
+      cin.ignore();
+      cin.getline(personas[i].nombre,50);
+      cout<< "Cedula: ";
+      cin.getline(personas[i].cedula,20);
+      cout<< "Edad: ";
       cin>>personas[i].edad;
-      cout<< "peso";
+      cout<< "Peso: ";
       cin>>personas[i].peso;
-      cout<< "altura";
+      cout<< "Altura: ";
       cin>>personas[i].altura;
-      cout<< "genero Masculino o Femenino";
+      cout<< "Genero Masculino o Femenino: ";
       cin>>personas[i].genero;
+      cin.ignore(100,'\n');
  }
 
  fstream codigo_binario("persona.dat", ios::out | ios::binary); 
@@ -40,9 +42,9 @@ data personas [number_persons];
  { 
     codigo_binario.write(reinterpret_cast<char*>(personas),number_persons*sizeof(data));
     codigo_binario.close();
-    cout<< "datos guardados en el arc: personas.dat \n";
+    cout<< "datos guardados en el arch: personas.dat \n";
  } else{
-    cout << "no se pudo abri el arc";
+    cout << "no se pudo abrir el arch: ";
 
 delete[]personas;
 return 0;
