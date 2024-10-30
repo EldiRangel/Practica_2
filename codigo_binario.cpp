e2e1233dee297279d4235599dd8107d0602f6901
eldi <eldi456.rangel@gmail.com>
2024-10-29 19:57:29 -0700

    parte 2

--- codigo_binario.cpp
+++ codigo_binario.cpp
@@ -15,9 +15,28 @@ int main(){
 int number_persons;
 cout <<"ingrese el numero de persoas:";
 cin.ignore();
-cin >> number_persons;
+cin>> number_persons;
 data personas [number_persons];
+ for (int i = 0; i < number_persons; i++)
+    { cout<< "persona"<< i+1<<".\n";
+      cout<< "id de; registo";
+      cin>>personas[i].id_registro;
+      cout<< "nombre"<< endl;
+      cin.getline(personas[i].nombre,35);
+      cout<< "cedula"<< endl;
+      cin.getline(personas[i].cedula,9);
+      cout<< "edad";
+      cin>>personas[i].edad;
+      cout<< "peso";
+      cin>>personas[i].peso;
+      cout<< "altura";
+      cin>>personas[i].altura;
+      cout<< "genero Masculino o Femenino";
+      cin>>personas[i].genero;
+ }
 
+ fstream codigo_binario("persona.dat", ios::out | ios::binary); 
+ 
 
 
 
